49317044a8fbda8e2171b5b35255a79c6c0a3f48
David Hanley <61337625+hanley2d@users.noreply.github.com>
2022-07-21 17:00:32 -0600

    Update post_repost.php

--- post_repost.php
+++ post_repost.php
@@ -12,7 +12,7 @@
             $post_date = date('Y-m-d H:i:s'); 
             $repost = $_POST["repost"];                       
 
-            $db = new mysqli("localhost", "hanley2d", "th3ori3s", "hanley2d");
+            $db = new mysqli("localhost", "hanley2d", "password", "hanley2d");
             if ($db->connect_error) {
                 die ("Connection failed: " . $db->connect_error);
             }   
